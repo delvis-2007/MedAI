1a5209879a21fa87bcfc2a8d1f1a3100b69ba280
delvis-2007 <ogenchedelvis@gmail.com>
2026-05-01 22:08:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import google.generativeai as genai
 
 app = Flask(__name__)
 
-genai.configure(api_key="GEMINI_API_KEY"))
+genai.configure(api_key="GEMINI_API_KEY")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 @app.route("/")
